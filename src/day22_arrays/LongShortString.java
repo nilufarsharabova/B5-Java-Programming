fd95833105153ac9ba87cb41bedcdcfd32de4137
nilufar sh <nilufar.sharabova@gmail.com>
2025-06-27 10:52:15 -0400

    Day22_Arrays

---
+++ src/day22_arrays/LongShortString.java
@@ -0,0 +1,30 @@
+package day22_arrays;
+
+public class LongShortString {
+    public static void main(String[] args) {
+        String [] words = {"java", "softskills", "selenium", "mentors", "quiz"};
+        System.out.println("Size of words: " + words.length);
+
+        String longest = words[0];  // "java"
+        String shortest = words[0]; // "java"
+
+
+        for ( String eachElem : words){ // ["java", "softskills", "selenium", "mentors", "quiz"]
+
+            if (eachElem.length() > longest.length()) {
+                longest = eachElem;
+            }
+
+            if (eachElem.length() < shortest.length()){
+                shortest = eachElem;
+            }
+
+        }
+
+        System.out.println("Longest word: " + longest);
+        System.out.println("Shortest word: " + shortest);
+
+
+
+    }
+}
